f2402a6f342ea9ab99277600626b99583b151983
toastmanAu <81925564+toastmanAu@users.noreply.github.com>
2026-03-01 17:02:40 +1030

    chore: gitignore .pio/ build cache in examples/minimal_watch

--- examples/minimal_watch/.pio/build/esp32dev/libc07/CKB-ESP32/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b.c.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/libc07/CKB-ESP32/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b.c.o: \
- /home/phill/workspace/CKB-ESP32/src/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b.c \
- /home/phill/workspace/CKB-ESP32/src/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2.h \
- /home/phill/workspace/CKB-ESP32/src/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2b/blake2-impl.h
